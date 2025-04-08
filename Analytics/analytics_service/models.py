6d6da9cb331fd25b8961b35b6585e1ae5f79ffbe
MohdZubair9 <mohdzubairali2002@gmail.com>
2025-04-08 17:32:29 +0530

    third commit

--- Analytics/analytics_service/models.py
+++ Analytics/analytics_service/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
 class AnalyticsEvent(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
